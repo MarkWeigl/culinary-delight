86c3123b6cd4452821e1330b5ef09834c7e646df
Mark Weigl <markwco@icloud.com>
2017-08-09 19:59:10 -0600

    adding items to recipe form

--- src/components/recipe-form.js
+++ src/components/recipe-form.js
@@ -13,8 +13,28 @@ export class RecipeForm extends Component {
   render() {
     return (
       <form onSubmit={e => addRecipe(e)}>
-        <input type="text" id="title" ref={input => this.title = input}/>
-        <button type="submit" className="btn btn-primary">Submit</button>
+     {/*}   <input type="text" id="title" ref={input => this.title = input}/>  */}
+                <div className="new-recipe-header">
+                    <p className="m-font">Add a Recipe</p>
+                    <p className="close-icon" onClick={this.RecipeForm}></p>
+                </div>
+                <hr />
+                <div className="new-recipe-body">
+                    <div>
+                        <p>Recipe</p>
+                        <input type="text"
+                            placeholder="Recipe Name"
+                            value={this.state.inputText }
+                            onChange={(event) => this.setState({ inputText: event.target.value })}
+                        />
+                    </div>
+                </div>
+                    <div className="new-recipe-footer">
+                        <button type="submit" className="add-recipe-button">Add a Recipe</button>
+                        <button className="edit-btn" onClick={this.RecipeForm}>Close</button>
+                    </div>
+                </form>
+    <button type="submit" className="btn btn-primary">Submit</button>
       </form>
     )
   }
