6e15886b8445e6f20b8faf0969552100004fba86
Mark Weigl <markwco@icloud.com>
2017-12-01 15:25:53 -0700

    added splash styling

--- src/components/recipe-delete.js
+++ src/components/recipe-delete.js
@@ -8,7 +8,6 @@ export function DeleteRecipe(_id) {
   deleteRecipe(_id) {
       this.props.dispatch(deleteRecipe(_id));
 
-
   render() {
     const onClick={this.props.handleClick(req => this.deleteRecipe(_id))}>
     return <button className="btn btn-primary">Delete</button>
