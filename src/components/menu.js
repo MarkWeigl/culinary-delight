99392e9f53802908abf61861d6f9b27cd187cff0
Patrick Ford <patrickford@msn.com>
2017-12-27 19:57:37 -0800

    Menu component and css

--- src/components/menu.js
+++ src/components/menu.js
@@ -7,28 +7,47 @@ import {clearAuth} from '../actions/auth';
 import {clearAuthToken} from '../local-storage';
 
 export class Menu extends React.Component {
-    logOut() {
-        this.props.dispatch(clearAuth());
-        clearAuthToken();
-    }
+  logOut() {
+      this.props.dispatch(clearAuth());
+      clearAuthToken();
+  }
+    
+  render() {
+    let logOutButton;
 
-    render() {
-        // Only render the log out button if we are logged in
-        let logOutButton;
-        if (this.props.loggedIn) {
-            logOutButton = (
-                <button onClick={() => this.logOut()}>Log out</button>
-            );
-        }
-        return (
-            <div className="menu">
-                <h1>Culinary Delight</h1>
-                {logOutButton}
-            </div>
-        );
-    }
+    if (this.props.loggedIn) {
+      logOutButton = (
+        <li className="menuItem logOut" onClick={() => this.logOut()}>Log out</li>
+      );
+    } 
+
+    return (
+    // Only render the log out button if we are logged in
+      <div className="menu">
+        <ul className="menuList">
+          <li className="menuItem">
+            <Link to={`/recipe-list`}>
+              Recipe List
+            </Link>
+          </li>
+          <li className="menuItem">
+            <Link to={`/add-recipe`}>
+              Add Recipe
+            </Link>
+          </li>
+          {logOutButton}
+        </ul>
+
+      </div>
+    );
+  }
 }
-//export default function Menu(props) 
+
+const mapStateToProps = state => ({
+  loggedIn: state.auth.currentUser
+});
+
+export default connect(mapStateToProps)(Menu);
   
 
 
