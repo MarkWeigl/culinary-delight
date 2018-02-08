3fd590a9be5054b6aa400d536934236a832b1de7
Mark Weigl <markwco@icloud.com>
2018-02-07 20:45:25 -0700

    debugged url in config.js

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 exports.API_BASE_URL = process.env.API_BASE_URL ||
-                      'https://stormy-mesa-11961.herokuapp.com/';
+                      'https://stormy-mesa-11961.herokuapp.com';
